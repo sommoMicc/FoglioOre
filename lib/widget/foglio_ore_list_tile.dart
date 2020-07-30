5fa652fdcdf0e56930eb50f8a45f8f91da7bb18d
Michele Tagliabue <mikitg.michele@gmail.com>
2020-07-30 12:12:31 +0200

    Inserita possibilità di visualizzare foglio ore già salvato

--- lib/widget/foglio_ore_list_tile.dart
+++ lib/widget/foglio_ore_list_tile.dart
@@ -3,7 +3,9 @@ import 'package:foglio_ore/model/cantiere.dart';
 import 'package:foglio_ore/model/lavoro.dart';
 import 'package:foglio_ore/model/stato.dart';
 import 'package:foglio_ore/screen/compilazione_page.dart';
+import 'package:foglio_ore/screen/pdf_preview.dart';
 import 'package:foglio_ore/utils/constants.dart';
+import 'package:foglio_ore/utils/pdf_utils.dart';
 import 'package:foglio_ore/utils/utils.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -40,30 +42,50 @@ class FoglioOreListTile extends StatelessWidget {
             ),
           ),
           ButtonBar(
+            mainAxisSize: MainAxisSize.max,
+            alignment: MainAxisAlignment.spaceBetween,
             children: [
+              FlatButton.icon(
+                color: AppColors.primary,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                onPressed: () async {
+                  _setAppState(context);
+                  var pdf = await PDFUtils.generatePDF(foglioOre.dati);
+
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => PDFPreview(pdf)));
+                },
+                icon: Icon(Icons.picture_as_pdf),
+                label: Text("Visualizza"),
+              ),
               FlatButton.icon(
                 color: AppColors.accent,
                 textColor: Colors.white,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10))),
                 onPressed: () {
-                  Provider.of<DateTimeAppState>(context, listen: false)
-                      .dataCorrente = foglioOre.dati.keys.first;
-
-                  Provider.of<GlobalAppState>(context, listen: false).dati =
-                      foglioOre.dati;
+                  _setAppState(context);
 
                   Navigator.of(context).push(MaterialPageRoute(
                       builder: (context) => CompilazionePage()));
                 },
                 icon: Icon(Icons.edit),
                 label: Text("Modifica"),
-              )
+              ),
             ],
           )
         ],
       ),
     );
+  }
+
+  void _setAppState(context) {
+    Provider.of<DateTimeAppState>(context, listen: false).dataCorrente =
+        foglioOre.dati.keys.first;
+
+    Provider.of<GlobalAppState>(context, listen: false).dati = foglioOre.dati;
   }
 
   static Widget _creaWidgetCantiere(
